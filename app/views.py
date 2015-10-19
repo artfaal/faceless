7ad01ee87e63d48b45ebcec1744ca7166e801825
Artfaal <sys.dll@gmail.com>
2015-10-19 14:28:33 +0300

    first step. Integrate to python

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from app import app
+from flask import render_template
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET'])
+@app.route('/index/', methods=['GET'])
+def index():
+    return render_template('index.html')
