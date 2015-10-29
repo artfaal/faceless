3f701ef7a765a818cbf3ad40bf7ed0a2cc71a268
Artfaal <sys.dll@gmail.com>
2015-10-29 22:48:07 +0300

    little refactoring

--- app/utility/import_db.py
+++ app/utility/import_db.py
@@ -3,12 +3,9 @@ import csv
 import sys
 from app import mongo
 from validators import *
-import time
-
 # Trick for normal unicode symbols
 reload(sys)
 sys.setdefaultencoding("utf-8")
-
 # Имя базы данных
 DB_NAME = 'name_of_the_base'
 
@@ -16,24 +13,11 @@ DB_NAME = 'name_of_the_base'
 FILENAME = 'tmp/pechi.csv'
 
 
-def makedic():
+def save_items_to_db():
     with open(FILENAME, 'rb') as f:
         reader = csv.reader(f, dialect='excel', delimiter=';')
-        count = 0
         for row in reader:
-            count += 1
             add = mongo.test.example.Items()
-            # print 'Имя: %s' % row[0]
-            # print 'Категории: %s | %s' % (row[1], row[2])
-            # print 'Описание: %s...' % row[3][:100]
-            # print 'Ключевики (SEO): %s...' % (row[4][:100])
-            # print 'Мета-Описание (SEO): %s...' % (row[5][:100])
-            # print 'Картинки: %s' % (row[6][:50])
-            # print 'Видео: %s' % (row[7][:100])
-            # print 'Инструкции: %s' % (row[8][:100])
-            # print 'Позиция: %s' % (row[9][:100])
-            # print 'Так же покупают: %s' % (row[10][:100])
-            # print '='*50
             add['name'] = row[0]
             add['slug'] = transliterate(row[0])
             add['category'] = row[2]
@@ -45,29 +29,6 @@ def makedic():
             add['doc'] = pars_img_doc_video(row[8])
             add['position'] = int_or_0(row[9])
             add.save()
-            # time.sleep(1)
-            print count
-
-        print count
-        sys.exit(1)
-
-
-def check_category():
-    with open(FILENAME, 'rb') as f:
-        reader = csv.reader(f, dialect='excel', delimiter=';')
-        list_of_cat = []
-        count_of_items = 0
-        for row in reader:
-            count_of_items += 1
-            if row[1] not in list_of_cat:
-                list_of_cat.append(row[1])
-
-            elif row[2] not in list_of_cat:
-                list_of_cat.append(row[2])
-
-        for i in list_of_cat:
-            print i
-        print '=' * 40 + '\n Всего полей просканированно: %s' % count_of_items
 
 
 def pars_img_doc_video(input):
@@ -87,12 +48,24 @@ def pars_img_doc_video(input):
             result.append({'filename': part[0], 'alt': part[1]})
         except IndexError:
             print 'Error! Check first line in CSV file'
-
     return result
 
 
-def save_to_db():
-    # Сохраняем всё в базу
-    add = mongo.test.example.Items()
-    add['name'] = 'testing'
-    print add
+def check_category():
+    # Вспомогательная функция, что бы визуально посмотреть,
+    # какие категории есть у товаров
+    with open(FILENAME, 'rb') as f:
+        reader = csv.reader(f, dialect='excel', delimiter=';')
+        list_of_cat = []
+        count_of_items = 0
+        for row in reader:
+            count_of_items += 1
+            if row[1] not in list_of_cat:
+                list_of_cat.append(row[1])
+
+            elif row[2] not in list_of_cat:
+                list_of_cat.append(row[2])
+
+        for i in list_of_cat:
+            print i
+        print '=' * 40 + '\n Всего полей просканированно: %s' % count_of_items
