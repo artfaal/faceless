e9c8927dcec4556b6bb14d15eb3e2b3f9b9bf19e
Artfaal <sys.dll@gmail.com>
2015-11-01 12:40:28 +0300

    make div clickable. and begin add child cat page

--- app/views.py
+++ app/views.py
@@ -9,9 +9,13 @@ def index():
 
 
 @app.route('/catalog/', methods=['GET'])
-def catalog():
+@app.route('/catalog/<slug>', methods=['GET'])
+def catalog(slug=None):
     category = mongo.test.category.find().sort('position')
     category2 = mongo.test.category.find().sort('position')
     category3 = mongo.test.category.find().sort('position')
+    name_of_category = mongo.test.category.find_one({'slug': slug})
 
-    return render_template('catalog.html', category=category, category2=category2, category3=category3)
+    return render_template('catalog.html', category=category,
+                           category2=category2, category3=category3, slug=slug, name_of_category=name_of_category)
+
