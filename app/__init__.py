4791f77c92d32002098c2bab9a36725485f637f6
Artfaal <sys.dll@gmail.com>
2015-11-02 00:47:55 +0300

    add markdown plugin and little more item category

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -
 from flask import Flask
 from mongokit import Connection
+from flaskext.markdown import Markdown
 
 app = Flask(__name__)
 app.config.from_object('config')
 mongo = Connection()
+Markdown(app)
 
 
 from app import views, models, utility
