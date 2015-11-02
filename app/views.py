0058e370d8934aada13d7189304dc4372e4f36e7
Artfaal <sys.dll@gmail.com>
2015-11-03 01:20:25 +0300

    revert some buggy changes

--- app/views.py
+++ app/views.py
@@ -27,10 +27,10 @@ def catalog(category_slug=None, item_slug=None):
     category_page = cat.find_one({'slug': category_slug})
 
     #  Выясняем, есть ли дочерние категории
-    if category_page:
-        def without_child():
-            if category_page['child_category'] == 0:
-                return True
+    # if category_page:
+    #     def without_child():
+    #         if category_page['child_category'] == 0:
+    #             return True
 
     # Страница продукта?
     if item_page:
@@ -41,13 +41,13 @@ def catalog(category_slug=None, item_slug=None):
                                item_page=item_page)
     #  Страница категории?
     elif category_page:
-        if without_child:
-            def items_from_category(child):
-                return items.find({'main_category': category_page['name']})
-        else:
-            def items_from_category(child):
-                return items.find({'main_category': category_page['name'],
-                                   'child_category': child})
+        # if without_child:
+        #     def items_from_category(child):
+        #         return items.find({'main_category': category_page['name']})
+        # else:
+        def items_from_category(child):
+            return items.find({'main_category': category_page['name'],
+                               'child_category': child})
         return render_template('catalog.html',
                                category=category,
                                category_slug=category_slug,
