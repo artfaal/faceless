9ca3caebb28d0d05c843a6510aefd633969801a6
Artfaal <sys.dll@gmail.com>
2015-11-03 09:57:48 +0300

    add iterable in with in main cat also

--- app/views.py
+++ app/views.py
@@ -26,12 +26,6 @@ def catalog(category_slug=None, item_slug=None):
     item_page = items.find_one({'slug': item_slug})
     category_page = cat.find_one({'slug': category_slug})
 
-    #  Выясняем, есть ли дочерние категории
-    # if category_page:
-    #     def without_child():
-    #         if category_page['child_category'] == 0:
-    #             return True
-
     # Страница продукта?
     if item_page:
         return render_template('catalog.html',
@@ -41,18 +35,19 @@ def catalog(category_slug=None, item_slug=None):
                                item_page=item_page)
     #  Страница категории?
     elif category_page:
-        # if without_child:
-        #     def items_from_category(child):
-        #         return items.find({'main_category': category_page['name']})
-        # else:
-        def items_from_category(child):
+        def items_from_child_category(child):
             return items.find({'main_category': category_page['name'],
                                'child_category': child})
+
+        def items_from_main_category():
+            return items.find({'main_category': category_page['name']})
+
         return render_template('catalog.html',
                                category=category,
                                category_slug=category_slug,
                                category_page=category_page,
-                               items_from_category=items_from_category)
+                               items_from_child_category=items_from_child_category,
+                               items_from_main_category=items_from_main_category)
     #  Значит catalog/
     else:
         return render_template('catalog.html',
