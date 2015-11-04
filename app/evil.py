1cf6e967fb9a93059edd6e1bf7f20117e602a7af
Artfaal <sys.dll@gmail.com>
2015-11-05 01:29:56 +0300

    fix typo

--- app/evil.py
+++ app/evil.py
@@ -12,18 +12,18 @@ def secret(token):
 
     if token == app.config['TOKEN_XLSX_TO_CSV']:
         run_convert()
-        return 'Run command convert xlsx to csv files'
+        return 'Convert xlsx to csv files in folder \n'
     elif token == app.config['REMOVE_CATEGORY_COLLECTION']:
         cat.drop()
-        return 'Drop CATEGORY collection'
+        return 'Remove all from "category" collection \n'
     elif token == app.config['REMOVE_ITEMS_COLLECTION']:
         items.drop()
-        return 'Drop ITEMS collection'
+        return 'Remove all from "items" collection \n'
     elif token == app.config['WRITE_TO_CATEGORY']:
         utility.import_db.save_category_to_db()
-        return 'Import Category from csv'
+        return 'Write in DB from "category" csv file \n'
     elif token == app.config['WRITE_TO_ITEMS']:
         utility.import_db.save_items_to_db()
-        return 'Import Category from csv'
+        return 'Write in DB from "items" csv files \n'
     else:
         return 'your token - \'%s\' didn\'t pass' % token
