5e73b1b757a3d7e8b6929e98ff2dc963139688de
Artfaal <sys.dll@gmail.com>
2015-11-11 15:55:04 +0300

    add link for quick jump to child cat

--- app/static/assets/js/main.js
+++ app/static/assets/js/main.js
@@ -8,6 +8,13 @@ $(document).ready(function() {
         $('#lightbox-shadow').css('display', 'block');
       }, function() {
         $('#lightbox-shadow').css('display', 'none');
-      }
+      };
     );
+    // Функция, что бы перескакивать на доп. категорию. Показывается только
+    // для групп, в которых есть дочерние
+    $('a[href*=#]').click(function(event){
+      event.preventDefault();
+      var target_top= $('a[name="'+this.href.split("#")[1]+'"]').offset().top;
+      $('html, body').animate({scrollTop:target_top}, 'slow');
+    });
 })
