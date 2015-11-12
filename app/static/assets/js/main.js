afefdfcbc65a1750c7d2d04fc71d38160ede39ef
Artfaal <sys.dll@gmail.com>
2015-11-12 13:43:12 +0300

    strange work of main.js. Some cycle..

--- app/static/assets/js/main.js
+++ app/static/assets/js/main.js
@@ -1,20 +1,26 @@
 // Мой пользовательский JS файл
 $(document).ready(function() {
-    // Функция для затемнения фона, при наведении на меню.
-    // Когда наводишь мышкой, активируется специальный фон, полупрозрачный
-    // который находится под меню, но над всем остальным
-    $( "#top-line-navigation" ).hover(
-      function() {
-        $('#lightbox-shadow').css('display', 'block');
-      }, function() {
-        $('#lightbox-shadow').css('display', 'none');
-      }
-    );
-    // Функция, что бы перескакивать на доп. категорию. Показывается только
-    // для групп, в которых есть дочерние
-    $('a[href*=#]').click(function(event){
-      event.preventDefault();
-      var target_top= $('a[name="'+this.href.split("#")[1]+'"]').offset().top;
-      $('html, body').animate({scrollTop:target_top}, 'slow');
-    });
+    function a() {
+      // Функция для затемнения фона, при наведении на меню.
+      // Когда наводишь мышкой, активируется специальный фон, полупрозрачный
+      // который находится под меню, но над всем остальным
+      $( "#top-line-navigation" ).hover(
+        function() {
+          $('#lightbox-shadow').css('display', 'block');
+        }, function() {
+          $('#lightbox-shadow').css('display', 'none');
+        }
+      );
+    };
+    function b() {
+      // Функция, что бы перескакивать на доп. категорию. Показывается только
+      // для групп, в которых есть дочерние
+      $('a[href*=#]').click(function(){
+
+        var target_top= $('a[name="'+this.href.split("#")[1]+'"]').offset().top;
+        $('html, body').animate({scrollTop:target_top}, 'slow');
+      });
+    };
+    a.call();
+    b.call();
 })
