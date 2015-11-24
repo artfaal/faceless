d60fee0196c1cbc1c2bd192652fc18d8a9d52639
Artfaal <sys.dll@gmail.com>
2015-11-24 16:29:43 +0300

    add ASSETS

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,30 @@ from flask import Flask
 from mongokit import Connection
 from flaskext.markdown import Markdown
 from flask.ext.thumbnails import Thumbnail
+from flask.ext.assets import Environment, Bundle
 
 app = Flask(__name__)
 app.config.from_object('config')
 mongo = Connection()
 thumb = Thumbnail(app)
+assets = Environment(app)
 Markdown(app)
+
+js = Bundle('assets/vendor/jQuery/jquery-1.11.3.min.js',
+            'assets/vendor/holder-js/holder.js',
+            'assets/vendor/lightbox/js/lightbox.js',
+            'assets/vendor/basic-jquery-slider/js/bjqs-1.3.min.js',
+            'assets/js/main.js',
+            filters='jsmin', output='gen/packed.js')
+assets.register('js_all', js)
+
+css = Bundle('assets/vendor/skeleton/normalize.css',
+             'assets/vendor/skeleton/skeleton.css',
+             'assets/vendor/basic-jquery-slider/bjqs.css',
+             'assets/vendor/lightbox/css/lightbox.css',
+             'assets/vendor/font-awesome/css/font-awesome.css',
+             'assets/css/main.css',
+             filters='cssmin', output='gen/packed.css')
+assets.register('css_all', css)
 
 from app import views, models, utility, evil, forms
