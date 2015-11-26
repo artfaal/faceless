a3bb68585a00da1772b30ee6c9157227f18ea91d
Artfaal <sys.dll@gmail.com>
2015-11-26 11:47:55 +0300

    update deploy

---
+++ deploy/only_content_changes.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+source "/Users/Artfaal/Dropbox/.faceless_config/VARIABLES"
+
+
+echo 'Чистим от мусора директорию контента'
+cd $share_content_yad
+find . -name '*.DS_Store' -type f -delete
+dot_clean $share_content_yad/
+
+echo 'Синхронизируем папку контента'
+rsync -avzu --delete --progress -h --exclude 'thumb' $share_content_yad/ root@$ip_of_the_server:$path_to_project/app/static/content/
+
+echo "Заходим по SSH"
+ssh root@$ip_of_the_server > /dev/null << EOF
+
+echo "Качаем базу в виде xlsx"
+mkdir $path_to_project/tmp/ && cd $path_to_project/tmp/ && rm -r *
+curl $link_to_xlsx -o db.xlsx > /dev/null
+
+echo "Назначаем права"
+sudo chown -R www-data:www-data $path_to_project
+sudo chmod -R 777 $path_to_project/tmp/
+
+exit
+EOF
+
+echo "Удаленно пишем базу"
+curl $ip_of_the_server/$xls_to_csv
+curl $ip_of_the_server/$rm_cat
+curl $ip_of_the_server/$rm_items
+curl $ip_of_the_server/$rm_pages
+curl $ip_of_the_server/$write_cat
+curl $ip_of_the_server/$write_items
+curl $ip_of_the_server/$write_pages
