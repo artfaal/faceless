c56416a30a9039a378325832b4af055ca5f37b3c
Artfaal <sys.dll@gmail.com>
2015-12-11 10:00:46 +0300

    Little ref main.js

--- app/static/assets/js/main.js
+++ app/static/assets/js/main.js
@@ -1,6 +1,5 @@
 // Мой пользовательский JS файл
 $(document).ready(function() {
-    function a() {
       // Функция для затемнения фона, при наведении на меню.
       // Когда наводишь мышкой, активируется специальный фон, полупрозрачный
       // который находится под меню, но над всем остальным
@@ -11,16 +10,14 @@ $(document).ready(function() {
           $('#lightbox-shadow').css('display', 'none');
         }
       );
-    };
-    function b() {
-      // Функция, что бы перескакивать на доп. категорию. Показывается только
-      // для групп, в которых есть дочерние
-      $('a[href*=#]').click(function(){
+});
 
-        var target_top= $('a[name="'+this.href.split("#")[1]+'"]').offset().top;
-        $('html, body').animate({scrollTop:target_top}, 'slow');
-      });
-    };
-    a.call();
-    b.call();
+$(document).ready(function() {
+  // Функция, что бы перескакивать на доп. категорию. Показывается только
+  // для групп, в которых есть дочерние
+  $('a[href*=#]').click(function(){
+
+    var target_top= $('a[name="'+this.href.split("#")[1]+'"]').offset().top;
+    $('html, body').animate({scrollTop:target_top}, 'slow');
+  });
 })
