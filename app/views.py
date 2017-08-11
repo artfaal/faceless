49082fd31c94082c41b50bfa807ff9415ecc60ca
Max Solovyev <sys.dll@gmail.com>
2017-08-11 15:09:14 +0300

    clean stuff and change path

--- app/views.py
+++ app/views.py
@@ -154,7 +154,7 @@ def news_list():
                            news=news)
 
 
-@app.route('/news2/', methods=['GET'])
+@app.route('/news_category/', methods=['GET'])
 def news_list2():
     recent_news = n.find().sort('position', -1).limit(4)
     return render_template('news_list2.html',
@@ -162,7 +162,7 @@ def news_list2():
                            pages=pages,
                            recent_news=recent_news)
 
-@app.route('/news2/<slug>', methods=['GET'])
+@app.route('/news_category/<slug>', methods=['GET'])
 def news_list2_section(slug):
     news = n.find({"section": slug}).sort('position', -1)
     return render_template('news_list.html',
